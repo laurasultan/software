c39e678dcbf7eaeddd500182289ea95f7a6e474a
laura <105574779+laurasultan@users.noreply.github.com>
2023-06-29 12:01:16 -0300

    Venda

---
+++ VendaManha/src/model/Cliente.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.util.Date;
+
+public class Cliente {
+    private long idCliente;
+    private String nomeCliente;
+    private Date nasc;
+    private String cpf;
+    private String telefone;
+    private String email;
+
+    public long getIdCliente() {
+        return this.idCliente;
+    }
+
+    public void setIdCliente(long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public Date getNasc() {
+        return nasc;
+    }
+
+    public void setNasc(Date nasc) {
+        this.nasc = nasc;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
