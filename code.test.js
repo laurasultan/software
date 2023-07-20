c690f8a414753c6f189e0cf994de81c9570ce52c
laura <105574779+laurasultan@users.noreply.github.com>
2023-07-20 11:47:05 -0300

    Delete code.test.js

--- code.test.js
+++
@@ -1,9 +0,0 @@
-const {somar, contaCaracter} = require("./code");
-
-it("SOMAR DOIS NÚMEROS", ()=>{
-    expect(somar(100, 200)).toBe(300);
-});
-
-it("CONTAR NÚMERO DE CARATERES", ()=>{
-    expect(contaCaracter("Chaves")).toBe(6);
-});
